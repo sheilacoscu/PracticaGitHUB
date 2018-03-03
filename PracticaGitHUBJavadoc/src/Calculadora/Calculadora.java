552ca26c1c15b10fa4ff253dbced3fb07daa0bc2
sheila_cosculluela <sheila_cosculluela@MintEntornos>
2018-03-03 14:06:59 +0100

    Modificado añadiendo las funciones potencia y tostring en calculadora y en Programa sacandolas por pantalla

--- PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
@@ -11,29 +11,46 @@ public class Calculadora {
 		this.op2 = op2;
 	}
 	
-	double suma() {
+	public double suma() {
 		return op1+op2;
 	}
 	
-	double resta() {
+	public double resta() {
 		return op1-op2;
 	}
 	
-	double division() {
+	public double division() {
 		if(op2==0) {
 			throw new IllegalArgumentException("El divisor es 0");
 		}
 		return op1/op2;
 	}
 	
-	//TODO: Añadir la operación potencia, que devuelve
+	public double potencia() {
+		if(op1 == 0) {
+			return 1;
+		}
+		if(op2<0) {
+			return -Math.pow(op1, op2);
+		}
+		return Math.pow(op1, op2);
+	}
+
+	@Override
+	public String toString() {
+		return "Calculadora de Sheila[operando1= " + op1 + ", operando2= " + op2 + "]";
+	}
+	
+	
+	
+	//TODO: Aï¿½adir la operaciï¿½n potencia, que devuelve
 	//op1 elevado a op2 con las siguientes reglas: 
-	//-Por convenio cualquier número elevado a 0 es igual a 1.
-	//-Un número "n" elevado a una potencia negativa "-p" es igual a 1/(n^p)
-	//Así 232^0 = 1; y 2^(-3)=1/2^3=1/8
+	//-Por convenio cualquier nï¿½mero elevado a 0 es igual a 1.
+	//-Un nï¿½mero "n" elevado a una potencia negativa "-p" es igual a 1/(n^p)
+	//Asï¿½ 232^0 = 1; y 2^(-3)=1/2^3=1/8
 	
 	
-	//TODO: Añadir la operacion toString que devuelva los operandos
+	//TODO: Aï¿½adir la operacion toString que devuelva los operandos
 	//la cadena:
 	//"Calculadora de <vuestroNombre>: "
 	//"operando1 = <op1>"
--- PracticaGitHUBJavadoc/src/Calculadora/Programa.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -1,11 +1,19 @@
 package Calculadora;
 
 public class Programa {
-	
+		public static void main(String[] args) {
+	Calculadora Oper = new Calculadora(9,5);
+		System.out.println("Suma = "+Oper.suma());
+		System.out.println("Resta = "+ Oper.resta());
+		System.out.println("Division = "+ Oper.division());
+		System.out.println("Potencia = "+ Oper.potencia());
+		System.out.println(Oper.toString());
+	}
+
 	//TODO: Crear un programa principal que 
 	//utilice todas las funciones de la calculadora
 	//Incluida las operaciones toString y la potencia
-	//que habréis añadido.
+	//que habrï¿½is aï¿½adido.
 	//Ejecutadlo.
 
 }
