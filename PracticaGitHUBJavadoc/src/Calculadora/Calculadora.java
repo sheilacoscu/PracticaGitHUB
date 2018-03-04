aef8d3af69e250b964155ecc26c947e1ee26cf1f
sheila_cosculluela <sheila_cosculluela@MintEntornos>
2018-03-04 10:15:59 +0100

    javadoc hemos creado javadoc en el proyecto

--- PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
@@ -1,67 +1,71 @@
 package Calculadora;
 
 public class Calculadora {
-
+/**
+ * Declaramos  op1 y op2
+ * @version 04/03/2018  
+ */
 	double op1;
 	double op2;
-	/**
-	 * Asignamos los numeros.
-	 * @param op1 
-	 * @param op2 
-	 */
-	
+
 	public Calculadora(double op1, double op2) {
+		/**
+		 * Creaccion del constructor
+		 * @param op1, op2: los parametros op1 y op2
+		 */
 		this.op1 = op1;
 		this.op2 = op2;
-		/**
-		 * montamos el numero
-		 */
+
 	}
 	
 	public double suma() {
 		/**
-		 * vamos a sumar operando1 + el operando2 
-		 * @return op1 + op2: muestra la suma
+		 * muestra la suma
+		 * @return op1+op2: muestra la suma de los dos operadores
 		 */
+
 		return op1+op2;
 	}
 	
 	
 	public double resta() {
 		/**
-		 * vamos a restar operando1 - operando2
-		 * @return op1-op2: muestra resta
+		 * muestra resta
+		 * @return op1-op2: muestra resta de los operadores
 		 */
 		return op1-op2;
 		
 	}
 	
 	public double division() {
-		/**
-		 * vamos a dividir el operando1 del operando2
-		 * @return op1/op2: muestra division
-		 */
+		
 		if(op2==0) {
 			throw new IllegalArgumentException("El divisor es 0");
 			/**
-			 * @trhow se produce un error si el divide por 0
+			 * @throw muestra un error si el divisor es 0
 			 */
 		}
 		return op1/op2;
-		
+		/**
+		 * muestra division del op1 y op2
+		 * @return op1/op2: muestra division
+		 */
 	}
 	
 	public double potencia() {
 		if(op1 == 0) {
 			return 1;
+				/**
+				 * si el resultado es 1 muestre 0
+				 */
 		}
 		if(op2<0) {
 			return -Math.pow(op1, op2);
 		}
 		return Math.pow(op1, op2);
 		/**
-		 *lo elevamos
-		 *@return Math.pow(op1, op2): muestra la potencia
+		 * muestra el numero elevado
+		 * @return Math.pow(op1,op2): muestra el numero elevado
 		 */
 	}
 
@@ -69,7 +73,7 @@ public class Calculadora {
 	public String toString() {
 		return "Calculadora de Sheila[operando1= " + op1 + ", operando2= " + op2 + "]";
 		/**
-		 * @return muestra el autor y los numeros que estan operando
+		 * @return muestra el autor de la calculadora y los operadores.
 		 */
 	}
 	
